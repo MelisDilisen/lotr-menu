b3d57ad83c7189c03e202f66765d8cd2baef4d8a
melisdilisen <melisdilisen@gmail.com>
2024-04-25 00:22:01 +0300

    add data.js and images

---
+++ public/data.js
@@ -0,0 +1,45 @@
+const foodData = [
+  {
+    name: "Lembas Bread",
+    ingredients:
+      "Traveler's sustenance made by elves, impervious to corruption",
+    price: 5,
+    photoName: "food/bread.png",
+    soldOut: false,
+  },
+  {
+    name: "Samwise's Stout Stew",
+    ingredients: "A hearty stew made with Hobbiton's finest beef",
+    price: 12,
+    photoName: "food/stew.png",
+    soldOut: false,
+  },
+  {
+    name: "Hobbiton Mushroom Caps",
+    ingredients: "Hobbit style mushroom toast",
+    price: 9,
+    photoName: "food/toast.png",
+    soldOut: false,
+  },
+  {
+    name: "Bilbo's Bountiful Pie",
+    ingredients: "Chicken, mushrooms, and leeks",
+    price: 10,
+    photoName: "food/pie.png",
+    soldOut: false,
+  },
+  {
+    name: "Beorn's Honeycake",
+    ingredients: "Cake with honey made from Beorn’s bee",
+    price: 3,
+    photoName: "food/cake.png",
+    soldOut: true,
+  },
+  {
+    name: "Ranger's Roast Chicken:",
+    ingredients: "A succulent chicken marinated in herbs and lemon",
+    price: 10,
+    photoName: "food/chicken.png",
+    soldOut: false,
+  },
+];
